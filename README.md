2af6f3674c6a7df95b9fd0a29c9acc1bd2367a20
“Siyanda <siyandakopolo2@gmail.com>
2023-06-14 16:34:49 +0200

    our first code

---
+++ README.md
@@ -0,0 +1,4 @@
+# Our First lesson
+
+we are trying to learn git for the first time.
+Let us hope its enjoy.
---
+++ gitlearn.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var greeting = "Hello, GIT"
+print(greeting)
+
+func sum(_ num1: Int, _ num2: Int) -> Int{
+    return num1 + num2
+}
+print(sum(10, 15))
+  
