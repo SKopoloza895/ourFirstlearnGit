1fcc8a47482710afeaee8f64b4a94f4db5585fc3
“Siyanda <siyandakopolo2@gmail.com>
2023-06-14 17:15:33 +0200

    added multiple for function

--- gitlearn.playground/Contents.swift
+++ gitlearn.playground/Contents.swift
@@ -11,3 +11,8 @@ func sum(_ num1: Int, _ num2: Int) -> Int{
 print(sum(10, 15))
 
 print("we are commit on stage")
+
+func multi(_ num1: Int, _ num2: Int) -> Int{
+    return num1 * num2
+}
+print(sum(5, 25))
